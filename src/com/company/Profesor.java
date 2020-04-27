271e585708804993fd40878ca9ba14bd685b0e1e
Patricio <plooferman@gmail.com>
2020-04-26 21:32:29 -0300

    Parte C

---
+++ src/com/company/Profesor.java
@@ -0,0 +1,48 @@
+package com.company;
+
+public class Profesor {
+
+    private String nombreProfesor;
+    private String apellidoProfesor;
+    private Integer antiguedadProfesor;
+    private Integer codigoProfesor;
+
+    public Profesor(String nombreProfesor, String apellidoProfesor, Integer antiguedadProfesor, Integer codigoProfesor) {
+        this.nombreProfesor = nombreProfesor;
+        this.apellidoProfesor = apellidoProfesor;
+        this.antiguedadProfesor = antiguedadProfesor;
+        this.codigoProfesor = codigoProfesor;
+    }
+
+    public String getNombreProfesor() {
+        return nombreProfesor;
+    }
+
+    public void setNombreProfesor(String nombreProfesor) {
+        this.nombreProfesor = nombreProfesor;
+    }
+
+    public String getApellidoProfesor() {
+        return apellidoProfesor;
+    }
+
+    public void setApellidoProfesor(String apellidoProfesor) {
+        this.apellidoProfesor = apellidoProfesor;
+    }
+
+    public Integer getAntiguedadProfesor() {
+        return antiguedadProfesor;
+    }
+
+    public void setAntiguedadProfesor(Integer antiguedadProfesor) {
+        this.antiguedadProfesor = antiguedadProfesor;
+    }
+
+    public Integer getCodigoProfesor() {
+        return codigoProfesor;
+    }
+
+    public void setCodigoProfesor(Integer codigoProfesor) {
+        this.codigoProfesor = codigoProfesor;
+    }
+}
