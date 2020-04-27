a7e54815ccc1e6503c54a82180484e3bc501069f
Patricio <plooferman@gmail.com>
2020-04-26 22:01:12 -0300

    Parte E

--- src/com/company/Curso.java
+++ src/com/company/Curso.java
@@ -1,13 +1,25 @@
 package com.company;
 
+import java.awt.*;
+import java.util.List;
+
 public class Curso {
 
     private String nombreCurso;
     private Integer codigoCurso;
 
-    public Curso(String nombreCurso, Integer codigoCurso) {
+    private ProfesorTitular profesorTitular;
+    private ProfesorAdjunto profesorAdjunto;
+    private Integer cupoAlumnos;
+    private List<Alumno> listaAlumnos;
+
+    public Curso(String nombreCurso, Integer codigoCurso, ProfesorTitular profesorTitular, ProfesorAdjunto profesorAdjunto, Integer cupoAlumnos, List<Alumno> listaAlumnos) {
         this.nombreCurso = nombreCurso;
         this.codigoCurso = codigoCurso;
+        this.profesorTitular = profesorTitular;
+        this.profesorAdjunto = profesorAdjunto;
+        this.cupoAlumnos = cupoAlumnos;
+        this.listaAlumnos = listaAlumnos;
     }
 
     public String getNombreCurso() {
@@ -24,5 +36,37 @@ public class Curso {
 
     public void setCodigoCurso(Integer codigoCurso) {
         this.codigoCurso = codigoCurso;
+    }
+
+    public ProfesorTitular getProfesorTitular() {
+        return profesorTitular;
+    }
+
+    public void setProfesorTitular(ProfesorTitular profesorTitular) {
+        this.profesorTitular = profesorTitular;
+    }
+
+    public ProfesorAdjunto getProfesorAdjunto() {
+        return profesorAdjunto;
+    }
+
+    public void setProfesorAdjunto(ProfesorAdjunto profesorAdjunto) {
+        this.profesorAdjunto = profesorAdjunto;
+    }
+
+    public Integer getCupoAlumnos() {
+        return cupoAlumnos;
+    }
+
+    public void setCupoAlumnos(Integer cupoAlumnos) {
+        this.cupoAlumnos = cupoAlumnos;
+    }
+
+    public List<Alumno> getListaAlumnos() {
+        return listaAlumnos;
+    }
+
+    public void setListaAlumnos(List<Alumno> listaAlumnos) {
+        this.listaAlumnos = listaAlumnos;
     }
 }
